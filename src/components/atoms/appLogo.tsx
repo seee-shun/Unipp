021d3fccd959d1f1006b82dcf1235c3efb5eaead
takahirokaji <tk69kj619@gmail.com>
2021-10-08 09:04:08 +0900

    add: application logo components

---
+++ src/components/atoms/appLogo.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import logoImage from "../../public/unipp.png";
+
+export const AppLogo: React.FC = () => {
+  return (
+    <>
+      <Image src={logoImage} alt="unipp Logo" width={92} height={38}></Image>
+    </>
+  );
+};
