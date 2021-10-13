d78e88a0cd85074ea704a6a21150e348b72a9773
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2021-10-13 08:06:02 +0000

    delete: css削除

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AppBase } from "../components/Layouts/AppBase";
