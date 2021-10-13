f2faa2bd41ba2e13d50e99bcd5adc90231d1164a
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2021-10-13 09:28:18 +0000

    add: テキストカラー追加

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+import {extendTheme} from '@chakra-ui/react'
+
+const theme = extendTheme({
+    styles:{
+        global:{
+            body:{
+                color:'#545454',
+            }
+        }
+    }
+  })
+
+  export default theme
