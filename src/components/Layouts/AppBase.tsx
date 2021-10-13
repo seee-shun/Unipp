cb17e8f57ca165140452d4e127f92b8081c2bd3c
takahirokaji <tk69kj619@gmail.com>
2021-10-13 04:25:07 +0000

    add: アプリのレイアウト

---
+++ src/components/Layouts/AppBase.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Box, Flex, Grid, GridItem } from "@chakra-ui/react";
+import { AppHeader as Header } from "../Organisms/AppHeader";
+
+export const AppBase: React.FC = ({ children }) => {
+  return (
+    <>
+      <Grid h="100vh" templateRows="auto 1fr">
+        <GridItem h="100%">
+          <Header />
+        </GridItem>
+        <GridItem overflowY="scroll" h="100%">
+          <Box p="0 1rem" h="inherit">
+            <Flex justifyContent="center" alignItems="center">
+              {children}
+            </Flex>
+          </Box>
+        </GridItem>
+      </Grid>
+    </>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { AppBase } from "../components/Layouts/AppBase";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <AppBase>
+        <Component {...pageProps} />
+      </AppBase>
+    </ChakraProvider>
+  );
 }
-export default MyApp
+export default MyApp;
