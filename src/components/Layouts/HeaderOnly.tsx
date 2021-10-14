bc077c093c42c84097fcb31d69250bd2e14a9383
takahirokaji <tk69kj619@gmail.com>
2021-10-14 09:43:09 +0000

    add: ヘッダーのみのレイアウトの作成

---
+++ src/components/Layouts/HeaderOnly.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { AppHeader as Header } from "../Organisms/AppHeader";
+
+export const HeaderOnly: React.FC = ({ children }) => {
+  return (
+    <>
+      <Header />
+      {children}
+    </>
+  );
+};
