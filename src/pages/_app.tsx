5be4a898c585a2ae262a645939cfc9106945972e
takahirokaji <tk69kj619@gmail.com>
2021-10-14 09:43:22 +0000

    add: ヘッダーのみのレイアウトに差し替え

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
-import { AppBase } from "../components/Layouts/AppBase";
+import { HeaderOnly } from "../components/Layouts/HeaderOnly";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <AppBase>
+      <HeaderOnly>
         <Component {...pageProps} />
-      </AppBase>
+      </HeaderOnly>
     </ChakraProvider>
   );
 }
