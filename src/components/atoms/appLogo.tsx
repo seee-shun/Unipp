d20463eb732275bbc6ad38bc327d8bb5173ea22f
takahirokaji <tk69kj619@gmail.com>
2021-11-13 22:58:30 +0900

    change: 型の付与

--- src/components/atoms/appLogo.tsx
+++ src/components/atoms/appLogo.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import Image from "next/image";
 import logoImage from "../../public/unipp.png";
 
-export const AppLogo: React.FC = () => {
+type Props = {
+  logo: StaticImageData;
+};
+
+export const AppLogo: React.FC<Props> = (props) => {
   return (
     <>
-      <Image src={logoImage} alt="unipp Logo" width={92} height={38}></Image>
+      <Image src={props.logo} alt="unipp Logo" width={92} height={38}></Image>
     </>
   );
 };
