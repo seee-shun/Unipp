8fc61eed811ac6b18ee0edd4913d391db626291a
takahirokaji <tk69kj619@gmail.com>
2021-11-14 00:47:55 +0900

    remove: 余分なコードの削除

--- src/components/Organisms/HeaderBack.tsx
+++ src/components/Organisms/HeaderBack.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { AppLogo } from "../atoms/appLogo";
 import { HeaderBase as Base } from "./HeaderBase";
 import { ChevronLeftIcon } from "@chakra-ui/icons";
 import { Spacer, Flex } from "@chakra-ui/layout";
 import { useRouter } from "next/router";
+
 export const HeaderBack: React.FC = (props) => {
   const router = useRouter();
   return (
