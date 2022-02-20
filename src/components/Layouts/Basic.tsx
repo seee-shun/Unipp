080209b82d76ce62d1364ce520050b1858c15a90
takahirokaji <tk69kj619@gmail.com>
2022-02-20 14:26:15 +0900

    fix: topのときだけ画面最大にするようにしました

--- src/components/Layouts/Basic.tsx
+++ src/components/Layouts/Basic.tsx
@@ -1,12 +1,16 @@
 import React from "react"
 import { Container } from "@chakra-ui/layout"
 import { HeaderBasic as Header } from "../Organisms/HeaderBasic"
+import { useRouter } from "next/router"
 
 export const Basic: React.FC = ({ children }) => {
+  const router = useRouter()
+  let container = "container"
+  container = router.pathname === "/" ? "container" : "container.lg"
   return (
     <>
       <Header />
-      <Container as="main" mt="0" p="0" maxW={{ lg: "container" }}>
+      <Container as="main" mt="0" p="0" maxW={{ lg: container }}>
         {children}
       </Container>
     </>
