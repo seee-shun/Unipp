2cc4840a1a8380e08118f4eaba35451c4eb4d261
takahirokaji <tk69kj619@gmail.com>
2022-02-20 12:59:56 +0900

    run: prettier

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -1,37 +1,38 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import { EventCard } from "../../components/Organisms/EventCard";
-import { EventSort } from "../../components/Organisms/EventSort";
-import { Box, Flex } from "@chakra-ui/layout";
-import { Button } from "@chakra-ui/react";
-import dayjs from "dayjs";
-import { firestore } from "../../libs/firebase";
-import { collection, getDocs, query, orderBy } from "firebase/firestore";
-import { useRouter } from "next/router";
+import type { NextPage } from "next"
+import Head from "next/head"
+import { EventCard } from "../../components/Organisms/EventCard"
+import { EventSort } from "../../components/Organisms/EventSort"
+import { Box, Flex } from "@chakra-ui/layout"
+import { IconButton } from "@chakra-ui/react"
+import { AddIcon } from "@chakra-ui/icons"
+import dayjs from "dayjs"
+import { firestore } from "../../libs/firebase"
+import { collection, getDocs, query, orderBy } from "firebase/firestore"
+import { useRouter } from "next/router"
 
 type Props = {
-  tags: string[];
+  tags: string[]
   events: [
     {
-      type: string;
-      host: string;
-      image: string;
-      title: string;
-      cliped: boolean;
-      finished?: boolean;
+      type: string
+      host: string
+      image: string
+      title: string
+      cliped: boolean
+      finished?: boolean
       period?: {
-        start: string;
-        finish: string;
-      };
+        start: string
+        finish: string
+      }
     }
-  ];
-};
+  ]
+}
 
 const Events: NextPage<Props> = (props) => {
-  const router = useRouter();
+  const router = useRouter()
   const pushToEventPost = () => {
-    router.push("event/post");
-  };
+    router.push("events/post")
+  }
 
   return (
     <>
@@ -41,12 +42,23 @@ const Events: NextPage<Props> = (props) => {
       </Head>
       <main>
         {/* <EventSort tags={props.tags} /> */}
-        <Box h="100%">
-          <Button onClick={pushToEventPost}></Button>
+        <Box h="calc(100vh - 80px)" overflowY="scroll">
+          <IconButton
+            colorScheme="pink"
+            aria-label="新規イベント追加"
+            onClick={pushToEventPost}
+            icon={<AddIcon />}
+            size="lg"
+            position="absolute"
+            borderRadius="full"
+            bottom={6}
+            zIndex={100}
+            right={{ base: "6", md: "33%", lg: "25%" }}
+          ></IconButton>
 
           <Flex flexWrap="wrap" justifyContent="justify-between" w="100%">
             {props.events.map((event, index) => {
-              // return <EventCard key={index} {...event} />;
+              return <EventCard key={index} {...event} />
             })}
             {props.events.map((event, index) => {
               // return <EventCard finished key={index} {...event} />;
@@ -61,24 +73,15 @@ const Events: NextPage<Props> = (props) => {
 export const getServerSideProps = async () => {
   // ここでfirebaseのコードを書く or Recoilのコード
 
-  const q = query(collection(firestore, "events"));
-  const EventsSnapshot = await getDocs(q);
+  const q = query(collection(firestore, "events"))
+  const EventsSnapshot = await getDocs(q)
   EventsSnapshot.forEach((doc) => {
-    console.log(doc.data());
-  });
+    console.log(doc.data())
+  })
 
-  const tags = [
-    "音楽",
-    "新歓",
-    "ゆる募",
-    "学会",
-    "サークル・イベント",
-    "勉強会",
-    "朝活",
-    "旅行",
-  ];
-  const start = new Date("December 17, 1995 03:24:00").toJSON();
-  const finish = new Date("December 17, 1995 03:24:00").toJSON();
+  const tags = ["音楽", "新歓", "ゆる募", "学会", "サークル・イベント", "勉強会", "朝活", "旅行"]
+  const start = new Date("December 17, 1995 03:24:00").toJSON()
+  const finish = new Date("December 17, 1995 03:24:00").toJSON()
   const events = [
     {
       type: "新歓",
@@ -179,10 +182,10 @@ export const getServerSideProps = async () => {
         finish: dayjs().format("MM月DD日"),
       },
     },
-  ];
+  ]
   return {
     props: { tags, events },
-  };
-};
+  }
+}
 
-export default Events;
+export default Events
