8c87dea926f9f912a992a7c25f63a2b3c6e57d62
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2022-02-20 06:29:47 +0000

    fix: 大学ドメインと一般メールの対応

--- src/pages/signUp/user.tsx
+++ src/pages/signUp/user.tsx
@@ -1,4 +1,5 @@
 import { ChangeEvent, useState } from "react"
+import Link from "next/link"
 import { useRecoilValue } from "recoil"
 import { IconButton } from "@chakra-ui/button"
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons"
@@ -8,19 +9,18 @@ import { Box, Divider, Flex, Heading, Stack } from "@chakra-ui/layout"
 import { useSignUp } from "../../hooks/useSignUp"
 import { univListState } from "../../store/univListState"
 import { PrimaryButton } from "../../components/atoms/button/PrimaryButton"
-import router from "next/router"
 
 const User = () => {
   const [userName, setUserName] = useState("")
-  const [email, setEmail] = useState("")
+  const [univEmail, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [showPassword, setShowPassword] = useState(false)
   const univList: any = useRecoilValue(univListState)
-  let univEmail = ''
+  let email = ''
   if (univList.address !== undefined) {
-    univEmail = `${email}${univList.address}`
+    email = `${univEmail}${univList.address}`
   } else {
-    univEmail = email;
+    email = univEmail;
   }
 
   const { signUp } = useSignUp()
@@ -29,13 +29,13 @@ const User = () => {
   const onChangeEmail = (e: ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)
   const onChangePassword = (e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)
   const handleClick = () => setShowPassword(!showPassword)
-  const onClickSignUp = () => signUp({ univEmail, password })
+  const onClickSignUp = () => signUp({ email, password })
 
   return (
     <Flex>
       <Box>
         <Heading as="h1" size="lg" textAlign="center">
-          新規作成
+          アカウントを作成
         </Heading>
         <Divider my={4} />
         <Stack spacing={4} py={4} px={10}>
@@ -43,7 +43,7 @@ const User = () => {
             <Input placeholder="ユーザ名" value={userName} onChange={onChangeUserName} />
           </InputGroup>
           <InputGroup>
-            <Input placeholder="大学メールアドレス" value={email} onChange={onChangeEmail} />
+            <Input placeholder="大学メールアドレス" value={univEmail} onChange={onChangeEmail} />
             <InputRightAddon>{univList.address}</InputRightAddon>
           </InputGroup>
           <InputGroup>
@@ -67,6 +67,7 @@ const User = () => {
           <PrimaryButton disabled={userName === "" || email === "" || password === ""} onClick={onClickSignUp}>
             新規作成
           </PrimaryButton>
+          <Link href='/signUp'>大学メールが違う場合はこちら</Link>
         </Stack>
       </Box>
     </Flex>
