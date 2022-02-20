8d840d9b1a429cbce4233a0386cd113fdf04f96d
takahirokaji <tk69kj619@gmail.com>
2022-02-20 13:19:17 +0900

    fix: topでアイコンが表示されないようにしました

--- src/components/Organisms/HeaderBasic.tsx
+++ src/components/Organisms/HeaderBasic.tsx
@@ -1,12 +1,15 @@
-import React from "react";
-import Link from "next/link";
-import { AppLogo } from "../atoms/appLogo";
-import { HeaderBase as Base } from "./HeaderBase";
-import { Avatar } from "@chakra-ui/react";
-import { Spacer, Flex } from "@chakra-ui/layout";
-import { useBreakpointValue } from "@chakra-ui/media-query";
+import React from "react"
+import Link from "next/link"
+import { AppLogo } from "../atoms/appLogo"
+import { HeaderBase as Base } from "./HeaderBase"
+import { Avatar } from "@chakra-ui/react"
+import { Spacer, Flex } from "@chakra-ui/layout"
+import { useBreakpointValue } from "@chakra-ui/media-query"
+import { useRouter } from "next/router"
 
 export const HeaderBasic: React.FC = (props) => {
+  const router = useRouter()
+  console.log(router.pathname)
   return (
     <>
       <Base>
@@ -14,16 +17,14 @@ export const HeaderBasic: React.FC = (props) => {
           <AppLogo />
         </Flex>
         <Spacer />
-        <Flex alignItems="center">
-          {/* マイページの作成は後で　kaji */}
-          <Link href="/mypage">
-            <Avatar
-              size={useBreakpointValue({ base: "sm", md: "md" })}
-              src="https://bit.ly/dan-abramov"
-            ></Avatar>
-          </Link>
-        </Flex>
+        {router.pathname !== "/" && (
+          <Flex alignItems="center">
+            <Link href="/mypage">
+              <Avatar size={useBreakpointValue({ base: "sm", md: "md" })} src="https://bit.ly/dan-abramov"></Avatar>
+            </Link>
+          </Flex>
+        )}
       </Base>
     </>
-  );
-};
+  )
+}
