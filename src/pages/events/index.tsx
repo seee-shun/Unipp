465c368e78735156ee1205116df733af9fd3e568
takahirokaji <tk69kj619@gmail.com>
2022-02-20 15:36:58 +0900

    fix

--- src/pages/events/index.tsx
+++ src/pages/events/index.tsx
@@ -32,48 +32,6 @@ const Events: NextPage<Props> = (props) => {
   const pushToEventPost = () => {
     router.push("events/post")
   }
-
-  return (
-    <>
-      <Head>
-        <title>Unipp:Events</title>
-        <meta name="description" content="Events of Unipp for every student" />
-      </Head>
-      <main>
-        <Box h="calc(100vh - 80px)" overflowY="scroll">
-          <IconButton
-            colorScheme="pink"
-            aria-label="新規イベント追加"
-            onClick={pushToEventPost}
-            icon={<AddIcon />}
-            size="lg"
-            position="absolute"
-            borderRadius="full"
-            bottom={6}
-            zIndex={100}
-            right={{ base: "4", lg: "8" }}
-          ></IconButton>
-
-          <Flex flexWrap="wrap" justifyContent="justify-between" w="100%">
-            {props.events.map((event, index) => {
-              return <EventCard key={index} {...event} />
-            })}
-          </Flex>
-        </Box>
-      </main>
-    </>
-  )
-}
-
-export const getServerSideProps = async () => {
-  // ここでfirebaseのコードを書く or Recoilのコード
-
-  // const q = query(collection(firestore, "events"))
-  // const EventsSnapshot = await getDocs(q)
-  // EventsSnapshot.forEach((doc) => {
-  //   console.log(doc.data())
-  // })
-
   const tags = ["音楽", "新歓", "ゆる募", "学会", "サークル・イベント", "勉強会", "朝活", "旅行"]
   const start = new Date("December 17, 1995 03:24:00").toJSON()
   const finish = new Date("December 17, 1995 03:24:00").toJSON()
@@ -178,9 +136,155 @@ export const getServerSideProps = async () => {
       },
     },
   ]
-  return {
-    props: { tags, events },
-  }
+
+  return (
+    <>
+      <Head>
+        <title>Unipp:Events</title>
+        <meta name="description" content="Events of Unipp for every student" />
+      </Head>
+      <main>
+        <Box h="calc(100vh - 80px)" overflowY="scroll">
+          <IconButton
+            colorScheme="pink"
+            aria-label="新規イベント追加"
+            onClick={pushToEventPost}
+            icon={<AddIcon />}
+            size="lg"
+            position="absolute"
+            borderRadius="full"
+            bottom={6}
+            zIndex={100}
+            right={{ base: "4", lg: "8" }}
+          ></IconButton>
+
+          <Flex flexWrap="wrap" justifyContent="justify-between" w="100%">
+            {events.map((event, index) => {
+              return <EventCard key={index} {...event} />
+            })}
+          </Flex>
+        </Box>
+      </main>
+    </>
+  )
 }
+
+// export const getServerSideProps = async () => {
+//   // ここでfirebaseのコードを書く or Recoilのコード
+
+//   // const q = query(collection(firestore, "events"))
+//   // const EventsSnapshot = await getDocs(q)
+//   // EventsSnapshot.forEach((doc) => {
+//   //   console.log(doc.data())
+//   // })
+//   const tags = ["音楽", "新歓", "ゆる募", "学会", "サークル・イベント", "勉強会", "朝活", "旅行"]
+//   const start = new Date("December 17, 1995 03:24:00").toJSON()
+//   const finish = new Date("December 17, 1995 03:24:00").toJSON()
+//   const events = [
+//     {
+//       type: "新歓",
+//       host: "おれ",
+//       image: "https://source.unsplash.com/user/erondu/1800x1500",
+//       title: "テスト",
+//       cliped: false,
+//       period: {
+//         start: dayjs().format("MM月DD日"),
+//         finish: dayjs().format("MM月DD日"),
+//       },
+//     },
+//     {
+//       type: "新歓",
+//       host: "未来研究所",
+//       image: "https://source.unsplash.com/user/erondu/1800x1501",
+//       title: "奇跡的ハッカソン",
+//       cliped: false,
+//       period: {
+//         start: dayjs().format("MM月DD日"),
+//         finish: dayjs().format("MM月DD日"),
+//       },
+//     },
+//     {
+//       type: "新歓",
+//       host: "おれ",
+//       image: "https://source.unsplash.com/user/erondu/1800x1501",
+//       title: "テスト",
+//       cliped: true,
+//       period: {
+//         start: dayjs().format("MM月DD日"),
+//         finish: dayjs().format("MM月DD日"),
+//       },
+//     },
+//     {
+//       type: "新歓",
+//       host: "おれ",
+//       image: "https://source.unsplash.com/user/erondu/1800x1503",
+//       title: "テスト",
+//       cliped: false,
+//       period: {
+//         start: dayjs().format("MM月DD日"),
+//         finish: dayjs().format("MM月DD日"),
+//       },
+//     },
+//     {
+//       type: "新歓",
+//       host: "おれ",
+//       image: "https://source.unsplash.com/user/erondu/1800x1504",
+//       title: "テスト",
+//       cliped: true,
+//       period: {
+//         start: dayjs().format("MM月DD日"),
+//         finish: dayjs().format("MM月DD日"),
+//       },
+//     },
+//     {
+//       type: "新歓",
+//       host: "おれ",
+//       image: "https://source.unsplash.com/user/erondu/1800x1505",
+//       title: "テスト",
+//       cliped: true,
+//       period: {
+//         start: dayjs().format("MM月DD日"),
+//         finish: dayjs().format("MM月DD日"),
+//       },
+//     },
+//     {
+//       type: "新歓",
+//       host: "おれ",
+//       image: "https://source.unsplash.com/user/erondu/1800x1506",
+//       title: "テスト",
+//       cliped: true,
+//       period: {
+//         start: dayjs().format("MM月DD日"),
+//         finish: dayjs().format("MM月DD日"),
+//       },
+//     },
+//     {
+//       type: "新歓",
+//       host: "おれ",
+//       image: "https://source.unsplash.com/user/erondu/1800x1507",
+//       title: "テスト",
+//       cliped: true,
+//       period: {
+//         start: dayjs().format("MM月DD日"),
+//         finish: dayjs().format("MM月DD日"),
+//       },
+//     },
+//     {
+//       type: "新歓",
+//       host: "おれ",
+//       image: "https://source.unsplash.com/user/erondu/1800x1508",
+//       title: "テスト",
+//       cliped: true,
+//       period: {
+//         start: dayjs().format("MM月DD日"),
+//         finish: dayjs().format("MM月DD日"),
+//       },
+//     },
+//   ]
+
+//   return {
+//     props: { tags, events },
+//   }
+// }
 
 export default Events
