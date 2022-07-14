e7fff4c53be6f900a60054467b1662ed61f0caff
工藤慣太 <kanta@KudoMacBook-Air-2.local>
2022-07-14 14:18:06 +0900

    add:タイトルを追加した

--- src/pages/reuse/post.tsx
+++ src/pages/reuse/post.tsx
@@ -1,15 +1,23 @@
 import { Box, Flex, Heading } from "@chakra-ui/layout"
+import type { NextPage } from "next"
 import Link from "next/link"
+import Head from "next/head"
 
-const ReusePost = () => {
+const ReusePost: NextPage = () => {
     return (
-        <Flex>
-            <Box>
-                <Heading as="h1" size="lg" textAlign="center">
-                    Unireuse Post
-                </Heading>
-            </Box>
-        </Flex>
+        <>
+            <Head>
+                <title>投稿ページ | Unireuse</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <Flex>
+                <Box>
+                    <Heading as="h1" size="lg" textAlign="center">
+                        Unireuse Post
+                    </Heading>
+                </Box>
+            </Flex>
+        </>
     )
 }
 
