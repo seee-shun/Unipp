7a7aa209d6f6e5195b57f9c1690fda605b89d9a8
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2022-07-14 05:18:03 +0000

    add: タイトルを追加しました

--- src/pages/reuse/details.tsx
+++ src/pages/reuse/details.tsx
@@ -1,14 +1,22 @@
+import type { NextPage } from "next"
+import Head from "next/head"
 import { Box, Flex, Heading } from "@chakra-ui/layout"
 
-const ReuseDetails = () => {
+const ReuseDetails: NextPage = () => {
     return (
-        <Flex>
-            <Box>
-                <Heading as="h1" size="lg" textAlign="center">
-                    Unireuse Detail
-                </Heading>
-            </Box>
-        </Flex>
+        <>
+            <Head>
+                <title>詳細ページ | Unireuse</title>
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <Flex>
+                <Box>
+                    <Heading as="h1" size="lg" textAlign="center">
+                        Unireuse Detail
+                    </Heading>
+                </Box>
+            </Flex>
+        </>
     )
 }
 
