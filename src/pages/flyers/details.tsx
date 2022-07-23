6d7b8006751c2aeda26509b49049c62fd97635d2
matsui-risa <matsui.risa.21@shizuoka.ac.jp>
2022-07-23 18:00:42 +0900

    fix: 要素をコンポーネント化しました

--- src/pages/flyers/details.tsx
+++ src/pages/flyers/details.tsx
@@ -26,16 +26,25 @@ const FlyerDetails: NextPage<Props> = () => {
   const posterIcon = "https://firebasestorage.googleapis.com/v0/b/unipp-develop.appspot.com/o/default_icon%2Fdf_ic_1.jpeg?alt=media&token=8cb1ea1a-1e0a-4005-9c6c-77c63f042d2e"
   // ここはfirebaseからのデータ取得時にstoreにデータを入れていないことが原因のエラー回避のため
   const createdAt = "22/7/23 16時24分"
+
+  const detailsList = [
+    { value :details.target, tag: "対象者"},
+    { value :details.contact, tag: "連絡先"},
+    { value: createdAt, tag: "投稿日"}
+  ]
+
   return (
     <>
-      <VStack w={"100vw"} align={"stretch"} 
-      display='flex' justifyContent='center' alignItems='center'>
-
+      <VStack
+        w={"100vw"}
+        align={"stretch"} 
+        display='flex'
+        justifyContent='center'
+        alignItems='center'>
       <Image src={details.imageURL} alt='selectedFlyerPicture' />
       
-      <Box >
-
-      <Flex w={"100%"}>
+      <Box pb={5}>
+        <Flex w={"100%"}>
           <Spacer />
           <Image
             src={posterIcon}
@@ -55,53 +64,29 @@ const FlyerDetails: NextPage<Props> = () => {
         <Box>
           <Text>{details.posterId}</Text>
         </Box>
-
-        <Box pt={5} pb={5} pr={2} 
-        w={"95vw"} backgroundColor={"blackAlpha.100"} borderRadius={"5px"} >
-          <Center>
-            <Text>{details.explain}</Text>
-          </Center>
-        </Box>
-
-        <Box pt={5}>
-          <Box pb={1}>
-            <Heading as='h2' size='sm'>対象者</Heading>
+          
+        <Box pt={5} pb={5} pr={2} w={"95vw"} backgroundColor={"blackAlpha.100"} borderRadius={"5px"} >
+        <Center>
+          <Text>{details.explain}</Text>
+        </Center>
           </Box>
-      
-          <Box pt={5} pb={5} pr={2} 
-          w={"95vw"} backgroundColor={"blackAlpha.100"} borderRadius={"5px"}>
-            <Center h='100%'>
-              <Text>{details.target}</Text>
-            </Center>
-          </Box>
-        </Box>
-
-        <Box pt={5}>
-          <Box pb={1}>
-            <Heading as='h2' size='sm'>連絡先</Heading>
-          </Box>
-
-          <Box pt={5} pb={5} pr={2}
-          w={"95vw"} backgroundColor={"blackAlpha.100"} borderRadius={"5px"}>
-            <Center h='100%'>
-              {details.contact}
-            </Center>
-          </Box>
-        </Box>
-
-        <Box  pt={5} pb={10}>
-          <Box pb={1}>
-          <Heading as='h2' size='sm'>投稿日</Heading>
-          </Box>
-          <Box pt={5} pb={5} pr={2}
-          w={"95vw"} backgroundColor={"blackAlpha.100"} borderRadius={"5px"}>
-            <Center h='100%'>
-              {createdAt}
-            </Center>
-          </Box>
-        </Box>
+          {detailsList.map((listItem) => (
+            <>
+              <Box pt={5}>
+                <Box pb={1}>
+                  <Heading as='h2' size='sm'>{listItem.tag}</Heading>
+                </Box>
+            
+                <Box pt={5} pb={5} pr={2} 
+                w={"95vw"} backgroundColor={"blackAlpha.100"} borderRadius={"5px"}>
+                  <Center h='100%'>
+                    <Text>{listItem.value}</Text>
+                  </Center>
+                </Box>
+              </Box>
+            </>
+          ))}
       </Box>
-
       </VStack>
     </>
   )
