f794b55d1a1024ecf34dfef6da5f541ec377d57c
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2022-07-24 04:20:09 +0900

    add bottom

--- src/next.config.js
+++ src/next.config.js
@@ -2,6 +2,6 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ["source.unsplash.com"],
+    domains: ["images.unsplash.com"],
   },
 }
--- src/pages/job/index.tsx
+++ src/pages/job/index.tsx
@@ -19,7 +19,13 @@ const JobPage: NextPage = () => {
             <meta name="description" content="Events of Unipp for every student" />
           </Head>
           <JobHeader />
-          index.tsxが消えてたから下メニューは復元できていない
+          <HStack>
+          <JobPost title='就活サイト' content='マイナビとかーー' link='/job/cite' img='https://images.unsplash.com/photo-1657788913414-2e6f7471f53b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=772&q=80' />
+          <Spacer />
+          <JobPost title='インターン' content='なかみいいい' link='/job/intern' img='https://images.unsplash.com/photo-1657700099187-9c30563d2e37?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60' />
+          <Spacer />
+          <JobPost title='適正検査' content='spiとかー' link='/job/spi' img='https://images.unsplash.com/photo-1657788912927-65fae63f8daf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw4fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60' />
+          </HStack>
         </Box>
       </Flex>
     </>
