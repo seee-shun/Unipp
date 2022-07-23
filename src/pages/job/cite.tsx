662be60882ed1670d79eee615896d787a4bcb560
gue <main@ghevp.com>
2022-07-24 03:28:36 +0900

    なんかした

--- src/pages/job/cite.tsx
+++ src/pages/job/cite.tsx
@@ -13,6 +13,27 @@ const JobPage: NextPage = () => {
           <meta name="description" content="Events of Unipp for every student" />
         </Head>
         <h1>サイト</h1>
+     
+        <p>ここでは有名な就活サイトを紹介します。</p>
+        <ul>
+          <li>
+            <h2>その１　マイナビ</h2>
+            <p>たぶん一番有名なとこです。とりあえず登録しましょう。</p>
+          </li>
+          <li>
+            <h2>その２　あさがくナビ</h2>
+            <p>なんかスカウト型の就活サイトらしいです。最近株価が伸びてるらしいのでなんかいい事があったんでしょう<br/>
+            
+          </li>
+          <li>
+            <h2>その３　キミスカ</h2>
+            <p>
+              F欄大学就職チャンネルで企業案件をしていた事で有名なチャンネルです。<br/>
+              詳しくは、下に記載している動画を見てください
+           </p>
+           <iframe width="560" height="315" src="https://www.youtube.com/embed/WdtmYtiplCw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </li>
+        </ul>
       </Box>
     </>
   )
--- src/pages/job/intern.tsx
+++ src/pages/job/intern.tsx
@@ -13,7 +13,26 @@ const JobPage: NextPage = () => {
           <meta name="description" content="Events of Unipp for every student" />
         </Head>
         <h1>インターン</h1>
-
+        <p>ここではインターンを行うために重要とされることを紹介します。</p>
+        <ul>
+          <li>
+            <h2>その１　カスじゃないエントリーシートを書く</h2>
+            <p>詳しくは、下に記載しているF欄大学就職チャンネルの動画をご覧ください</p>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/SR4l5AKVmR4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </li>
+          <li>
+            <h2>その２　終活サイトに登録する</h2>
+            <p>就職活動をするためには、就活サイトに登録することは当然のことです。<br/>
+            <b>早いうちに</b><a href="https://job.mynavi.jp/2024/">マイナビ</a>や<a href="https://www.gakujo.ne.jp/2024/?utm_source=yahoo&utm_medium=cpc&utm_campaign=YSS_camp&yclid=YSS.1000299442.EAIaIQobChMIvuDd98mP-QIVCzdgCh1csQa8EAAYASAAEgJRv_D_BwE">あさがくナビ</a>のような就活サイトにエントリーをしましょう。</p>
+          </li>
+          <li>
+            <h2>その３　とにかくインターンに応募しまくる！</h2>
+            <p>インターンに落ちてしまう事があるかもしれませんが、そんなときのためにもたくさんの企業のインターンを受けましょう！<br/>
+            もしも沢山受かってしまっても大丈夫！！<br/>
+            どんどん逆お祈りしちゃいましょう！！！
+            </p>
+          </li>
+        </ul>
       </Box>
     </>
   )
